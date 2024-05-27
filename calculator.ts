35c7aa955ba7deeff309b84dc944692e8b1ec458
Atkinss <jordanatkins@live.com>
2024-05-27 15:26:11 -0600

    add: try/catch block and use example of type narrowing

--- calculator.ts
+++ calculator.ts
@@ -15,4 +15,15 @@ const calculator = (a: number, b: number, op: Operation): number => {
   }
 }
 
-console.log(calculator(2, 4, 'add'));
+try { 
+  console.log(calculator(2, 4, 'add'));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong: ';
+  // using "instanceof" to narrow the type of error
+  if (error instanceof Error) {
+    // Here the type is narrowed and can refer to error.message
+    errorMessage += error.message;
+  }
+  // outside of the if-statement we can no longer reference error.message
+  console.log(errorMessage);
+}
