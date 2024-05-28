9fcc893ee25643207978a2261224c25192bc61e8
Atkinss <jordanatkins@live.com>
2024-05-28 17:44:01 -0600

    add: multiplier and calculator scripts, use CL args in multiplicator function

--- multiplier.ts
+++ multiplier.ts
@@ -2,3 +2,7 @@ const multiplicator = function (a: number, b: number, printText: string) {
   console.log(printText, a * b);
 }
 
+const a: number = Number(process.argv[2]);
+const b: number = Number(process.argv[3]);
+
+multiplicator(a, b, `Multiplied ${a} and ${b}, the result is:`);
