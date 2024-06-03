aa436bd382d449fd0b3aa3eb8593396a43223ebc
Atkinss <jordanatkins@live.com>
2024-06-02 19:33:20 -0600

    add: function validations

---
+++ helper functions/validateExerciseArguments.ts
@@ -0,0 +1,25 @@
+import { ExerciseArguments } from "../exercises/exerciseCalculator";
+
+export const parseExerciseArguments = function (daily_exercises: number[], target: number): ExerciseArguments {
+  
+  const message = `You have provided ${daily_exercises.length} arguments in your Array of hours, exerciseCalculator requires you to report hours for 7 days of the week and your daily target.`;
+
+  if (daily_exercises.length !== 7) throw new Error(message);
+
+  for (const hours of daily_exercises) {
+    if (isNaN(hours)) {
+      throw new Error(`${hours} is not a type Number. Please submit Numbers in the arguments.`);
+    }
+  }
+
+  const targetHours = Number(target);
+
+  if (isNaN(Number(targetHours))) {
+    throw new Error(`${target} is not a type Number. Please submit Numbers in the arguments.`);
+  }
+
+  return {
+    daily_exercises,
+    target
+  };
+};
